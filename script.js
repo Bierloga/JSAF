55accdfa99cd4f192a9e0bc6b693096ff73fa619
Lucas Derksen <luc.derksen@gmail.com>
2021-11-08 22:51:48 +0100

    actual script

---
+++ script.js
@@ -0,0 +1,21 @@
+// zet de functie om naar een arrow functie
+// const ikRockArrowFunctions = () => {
+//     console.log("Joe, ik rock de arrow functions!");
+// };
+
+// ikRockArrowFunctions()
+
+let fivePlusSeven = () => 5 + 7;
+
+console.log(fivePlusSeven());
+
+let myFunction = (a, b) => a + b;
+
+console.log(myFunction(3, 4));
+
+let addFive = a => a + 5;
+console.log(addFive(4));
+
+let createObject = () => ("greeting: 'hoi'");
+
+console.log(createObject());
